b167326c725a72c8cc5239bc2d0a66193e5c1623
aayambhatt <aayambhatt23@gmail.com>
2024-12-16 02:09:35 +0530

    file added

---
+++ Js-Fundamentals-1/SwitchStatement.js
@@ -0,0 +1,15 @@
+const day = 'monday';
+
+switch(day){
+    case 'monday':
+    console.log("Do DSA");
+    console.log("Do SQL");
+    break;
+
+    case 'Tuesday':
+        console.log("Do backend development");
+        break;
+    
+    default:
+        console.log("Revision");
+}
