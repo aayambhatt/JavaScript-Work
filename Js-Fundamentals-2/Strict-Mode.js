ac51b2fb22b0bcd1c272bc9434b0c627b6f7f445
aayambhatt <aayambhatt23@gmail.com>
2024-12-17 00:14:58 +0530

    file added

---
+++ Js-Fundamentals-2/Strict-Mode.js
@@ -0,0 +1,10 @@
+'use strict';
+
+let iAmDeveloper = false;
+let passTest = true;
+
+if(passTest) iAmDeveloper = true;
+if(iAmDeveloper) console.log("He can program");
+
+const interface = "program"; //SyntaxError: Unexpected strict mode reserved word
+
