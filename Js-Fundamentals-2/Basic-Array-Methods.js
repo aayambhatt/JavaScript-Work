914c224411b9a8083d4bbcd3e598ee6743fd7cea
aayambhatt <aayambhatt23@gmail.com>
2024-12-31 14:57:41 +0530

    files added

--- Js-Fundamentals-2/Basic-Array-Methods.js
+++ Js-Fundamentals-2/Basic-Array-Methods.js
@@ -1,2 +1,21 @@
 const langs = ['JavaScript', 'C++', 'Python', 'Go'];
 langs.push('SQL');
+
+// add element at first position 
+langs.unshift('Node JS');
+console.log(langs);
+
+// remove last element 
+langs.pop();
+console.log(langs);
+
+// remove first element
+langs.shift();
+console.log(langs);
+
+// check position 
+console.log(langs.indexOf('Python'));
+
+// modern method to check if element is there or not 
+console.log(langs.includes('JavaScript')); // true
+console.log(langs.includes('Java')); // false
