99ba0fa0885f1bfb5471112e271cdbddc7128af7
aayambhatt <aayambhatt23@gmail.com>
2025-01-24 11:52:56 +0530

    DOM

---
+++ DOM-JS/script.js
@@ -0,0 +1,17 @@
+// DOM = document object model 
+// 4 pillars : Selection of elements, changing html, changing css, event listener 
+
+// selection of elements
+var a = document.querySelector("h1");
+console.log(a);
+
+// changing html
+var b = document.querySelector("h1");
+b.innerHTML = "Change of h1"
+
+// changing css
+ a.style.color= "red";
+ a.style.backgroundColor = "black";
+
+ // event listener 
+ 
